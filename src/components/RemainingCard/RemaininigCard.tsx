12886d72459f9051c9ddb46ea80605fa8872f10b
111Bakhmat <k.bakhmat@mail.ru>
2023-04-17 22:33:15 +0300

    feat(p_4) add new components

---
+++ src/components/RemainingCard/RemaininigCard.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const RemaininigCard = () => {
+  return <div>RemaininigCard</div>;
+};
---
+++ src/types/types.ts
@@ -0,0 +1,6 @@
+import { Currency } from "../config/Currency";
+
+export interface IOption {
+  value: Currency;
+  label: string;
+}
